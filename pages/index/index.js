18a017e67a76b8eeef1db0d5bab331159817981e
monkeyHe <2498282923@qq.com>
2025-03-20 22:33:46 +0800

    3.20

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,29 @@
 // index.js
 Page({
   data:{
-    status:'none'
+    status:'none',
+    isshow:true
   },
   showCard1(){
     this.setData({
       status:"ruby"
+    })
+  },
+  onScroll(e){
+   let height =  e.detail.scrollTop
+   if(height >= 100){
+     this.setData({
+      isshow:false
+     })
+   }else{
+     this.setData({
+       isshow:true
+     })
+   }
+  },
+  goNet(){
+    wx.navigateTo({
+      url: '/pages/Pano/Pano',
     })
   }
 })
